2a67018af7df90a6601a2774ddabdde64e0dc546
Digital AGA <89376199+digitalaga@users.noreply.github.com>
2024-08-03 14:10:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(path.resolve(), 'public', 'index.html'));
 });
 
-app.get('/freechatgptonline', async (req, res) => {
+app.get('/gptchatly', async (req, res) => {
     const text = req.query.text;
     if (!text) return res.status(400).send('Text is required');
     try {
-        const result = await generate(text);
-        res.send(result.reply);
-    } catch (e) {
-        res.status(500).send(e.message);
+        const response = await getgptchatlyResponse(text);
+        res.send(response.chatGPTResponse);
+    } catch (error) {
+        res.status(500).send(error.message);
     }
 });
 
@@ -27,23 +27,32 @@ app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-async function generate(q) {
+function generateRandomIP() {
+    const octet = () => Math.floor(256 * Math.random());
+    return `${octet()}.${octet()}.${octet()}.${octet()}`;
+}
+
+async function getgptchatlyResponse(content) {
+    const headers = {
+        "Content-Type": "application/json",
+        "User-Agent": "Mozilla/5.0 (Linux; Android 11; M2004J19C Build/RP1A.200720.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.129 Mobile Safari/537.36 WhatsApp/1.2.3",
+        Referer: "https://gptchatly.com/",
+        "X-Forwarded-For": generateRandomIP()
+    };
+    const requestData = {
+        past_conversations: [{
+            role: "system",
+            content: "You are a helpful assistant."
+        }, {
+            role: "user",
+            content: content
+        }]
+    };
     try {
-        const { data } = await axios("https://www.freechatgptonline.com/wp-json/mwai-ui/v1/chats/submit", {
-            method: "post",
-            data: {
-                botId: "default",
-                newMessage: q,
-                stream: false
-            },
-            headers: {
-                Accept: "text/event-stream",
-                "Content-Type": "application/json"
-            }
-        });
-        return data;
-    } catch (err) {
-        console.log(err.response.data);
-        return { reply: err.response.data.message };
+        const response = await axios.post("https://gptchatly.com/fetch-response", requestData, { headers });
+        return response.data;
+    } catch (error) {
+        console.error("Error:", error);
+        throw error;
     }
 }
